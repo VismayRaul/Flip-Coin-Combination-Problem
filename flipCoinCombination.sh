af9188280465d7813cafb86da674191608cbc6c0
VismayRaul <vismayraul@gmail.com>
2020-11-09 23:45:24 +0530

    triplet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash 
 
-total=50;
+read -p "number of times flipping of coin to be done - " total;
 
 #for singlet
 declare -A result
@@ -52,3 +52,40 @@ echo percentage_HH=`echo $head_head | awk '{print $1/50*100}'`
 echo percentage_HT=`echo $head_tail | awk '{print $1/50*100}'`
 echo percentage_TH=`echo $tail_head | awk '{print $1/50*100}'`
 echo percentage_TT=`echo $tail_tail | awk '{print $1/50*100}'`
+
+#for triplet
+declare -A triplet
+
+for ((k=1;k<=$total;k++))
+do
+        triplet[$k]=$((RANDOM%8))
+done
+
+#count
+
+#0 for HHH
+echo HHH=`echo ${triplet[@]} | tr ' ' '\n' | sort -n | uniq -c | head -1 | awk '{print $1}'`
+#1 for HHT
+echo HHT=`echo ${triplet[@]} | tr ' ' '\n' | sort -n | uniq -c | head -2 | tail -1 | awk '{print $1}'`
+#2 for HTH
+echo HTH=`echo ${triplet[@]} | tr ' ' '\n' | sort -n | uniq -c | head -3 | tail -1 | awk '{print $1}'`
+#3 for THH
+echo THH=`echo ${triplet[@]} | tr ' ' '\n' | sort -n | uniq -c | head -4 | tail -1 | awk '{print $1}'`
+#4 for TTH
+echo TTH=`echo ${triplet[@]} | tr ' ' '\n' | sort -n | uniq -c | tail -4 | head -1 | awk '{print $1}'`
+#5 for THT
+echo THT=`echo ${triplet[@]} | tr ' ' '\n' | sort -n | uniq -c | tail -3 | head -1 | awk '{print $1}'`
+#6 for HTT
+echo HTT=`echo ${triplet[@]} | tr ' ' '\n' | sort -n | uniq -c | tail -2 | head -1 | awk '{print $1}'`
+#7 for TTT
+echo TTT=`echo ${triplet[@]} | tr ' ' '\n' | sort -n | uniq -c | tail -1 | awk '{print $1}'`
+
+#triplet percentage
+echo percentage_HHH=`echo $HHH | awk '{print $1/50*100}'`
+echo percentage_HHT=`echo $HHT | awk '{print $1/50*100}'`
+echo percentage_HTH=`echo $HTH | awk '{print $1/50*100}'`
+echo percentage_THH=`echo $THH | awk '{print $1/50*100}'`
+echo percentage_TTH=`echo $TTH | awk '{print $1/50*100}'`
+echo percentage_THT=`echo $THT | awk '{print $1/50*100}'`
+echo percentage_HTT=`echo $HTT | awk '{print $1/50*100}'`
+echo percentage_TTT=`echo $TTT | awk '{print $1/50*100}'`
